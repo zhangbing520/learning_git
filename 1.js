c3e9b42bf72caa46855efbc63286f1a0bffbb033
张兵 <jiaxuan@douyu.tv>
2022-08-17 22:52:30 +0800

    feat: modify func name

--- 1.js
+++ 1.js
@@ -3,8 +3,8 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 22:33:04
+ * @LastEditTime: 2022-08-17 22:51:57
  */
-const add = (a, b) => a + b;
+const ad = (a, b) => a + b;
 
-const multiply = (a, b) => a * b;
+const mul = (a, b) => a * b;
