1d3e05e51bfe833218a9f5fdd233dd4d34bde1ed
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:15:42 +0800

    feat: div sec

--- 1.js
+++ 1.js
@@ -3,8 +3,12 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 22:51:57
+ * @LastEditTime: 2022-08-17 23:13:44
  */
 const ad = (a, b) => a + b;
 
 const mul = (a, b) => a * b;
+
+const div = (a, b) => a / b;
+
+const dec = (a, b) => a - b;
