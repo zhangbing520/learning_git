ce146cd9d49ab42443df45c7b3f2d61aa490894d
张兵 <jiaxuan@douyu.tv>
2022-08-18 00:30:50 +0800

    xxx

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-18 00:15:43
+ * @LastEditTime: 2022-08-18 00:32:31
  */
 const ad = (a, b) => a + b;
 
@@ -19,3 +19,5 @@ const count = 1;
 const something = 'something';
 
 const something_add = 'something_aa';
+
+const xxx = 'xxx';
