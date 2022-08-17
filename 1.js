fe8765aff8b571c7d61270e25271aacd99e81f5d
张兵 <jiaxuan@douyu.tv>
2022-08-18 00:03:04 +0800

    feat: something delete xxx

--- 1.js
+++ 1.js
@@ -3,14 +3,16 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 23:49:16
+ * @LastEditTime: 2022-08-18 00:02:12
  */
-const adxxxX = (a, b) => a + b;
+const ad = (a, b) => a + b;
 
-const mulxxxx = (a, b) => a * b;
+const mul = (a, b) => a * b;
 
 const divooo = (a, b) => a / b;
 
 const decooo = (a, b) => a - b;
 
-const count = 1
+const count = 1;
+
+const something = 'something';
