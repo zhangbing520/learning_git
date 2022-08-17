1b4aa2fd4896f0406ed20a76b720afd9872fdb02
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:15:42 +0800

    feat: merge

--- 1.js
+++ 1.js
@@ -3,10 +3,12 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 23:18:56
+ * @LastEditTime: 2022-08-17 23:23:30
  */
 const add = (a, b) => a + b;
 
-const multiply = (a, b) => a * b;
+const mul = (a, b) => a * b;
 
-const division = (a, b) => a / b;
+const div = (a, b) => a / b;
+
+const dec = (a, b) => a - b;
