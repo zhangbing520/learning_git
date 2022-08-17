df3ff0618a17836a365cc50d187514f376404302
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:44:26 +0800

    feat: add test func

--- 1.js
+++ 1.js
@@ -3,8 +3,10 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 22:33:04
+ * @LastEditTime: 2022-08-17 23:44:06
  */
 const add = (a, b) => a + b;
 
 const multiply = (a, b) => a * b;
+
+const test = () => { console.log('test') };
