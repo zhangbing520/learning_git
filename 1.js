16b3ba392517934fa0f1e8eca900aed07421ed9a
张兵 <jiaxuan@douyu.tv>
2022-08-17 22:34:15 +0800

    feat: modify first commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Autor: zhangbing
+ * @Date: 2022-08-17 22:30:49
+ * @LastEditors: zhangbing
+ * @LastEditTime: 2022-08-17 22:33:04
+ */
+const add = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
