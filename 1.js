65d74abde90c33857348c0f8a423dab0be0df6a4
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:20:01 +0800

    feat: division

--- 1.js
+++ 1.js
@@ -3,8 +3,10 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 23:04:16
+ * @LastEditTime: 2022-08-17 23:18:56
  */
 const add = (a, b) => a + b;
 
 const multiply = (a, b) => a * b;
+
+const division = (a, b) => a / b;
