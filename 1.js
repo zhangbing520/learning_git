ae376c90bb52abd30cc78c8790d4dda4cb79cc40
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:50:46 +0800

    feat: add count

--- 1.js
+++ 1.js
@@ -3,12 +3,14 @@
  * @Autor: zhangbing
  * @Date: 2022-08-17 22:30:49
  * @LastEditors: zhangbing
- * @LastEditTime: 2022-08-17 23:13:44
+ * @LastEditTime: 2022-08-17 23:49:16
  */
-const ad = (a, b) => a + b;
+const adxxxX = (a, b) => a + b;
 
-const mul = (a, b) => a * b;
+const mulxxxx = (a, b) => a * b;
 
-const div = (a, b) => a / b;
+const divooo = (a, b) => a / b;
 
-const dec = (a, b) => a - b;
+const decooo = (a, b) => a - b;
+
+const count = 1
