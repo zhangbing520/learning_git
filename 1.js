128887fd062c8adaf630df34d49882bfa0f8caa8
张兵 <jiaxuan@douyu.tv>
2022-08-17 23:50:46 +0800

    feat: add count

---
+++ 1.js
@@ -0,0 +1,18 @@
+
+/*
+ * @Description: 
+ * @Autor: zhangbing
+ * @Date: 2022-08-17 22:30:49
+ * @LastEditors: zhangbing
+ * @LastEditTime: 2022-08-17 23:59:45
+ */
+const adxxxX = (a, b) => a + b;
+
+const mulxxxx = (a, b) => a * b;
+
+const divooo = (a, b) => a / b;
+
+const decooo = (a, b) => a - b;
+
+const count = 1
+
